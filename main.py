2724b05de83c09ba601c05eff86b54f41a96b964
ddiomedi2096 <ddiomedi2906@gmail.com>
2019-08-24 11:41:00 -0400

    modify main

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hellooooo")
+import requests
+
+def handle_json(data):
+    print(data)
+
+# Get last pulse generated
+beacon_url = "https://beacon.clcert.cl/beacon/2.0/pulse/last"
+content = requests.get(beacon_url)
+pulse = content.json()["pulse"]
+handle_json(pulse)
