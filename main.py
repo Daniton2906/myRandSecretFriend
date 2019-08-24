4e1c5b701ceb3e99363985f0b43ffd8b2ffa6eee
Javi801 <javi@niclabs.cl>
2019-08-24 13:26:24 +0200

    Date verification with the user

--- main.py
+++ main.py
@@ -10,6 +10,17 @@ amigos = ['a', 'b', 'c', 'd', 'e']
 # fecha para el sorteo en formato dd-mm-aaaa
 date = "20-06-2020"
 time = "20:00"
+
+# verificacion de la fecha
+correct = 0
+while (correct == 0):
+    numero = input("Desea realizar el sorteo en la fecha y hora siguientes?: \n" + date + " " + time + "\n")
+    if (correct):
+        break
+    date = raw_input("Ingresa la nueva fecha en formato dd-mm-aaaa:")
+    time = raw_input("Ingresa la nueva fecha en formato hh:mm :")
+
+# TODO verificacion de formato
 content = requests.get(beacon_url)
 pulse = content.json()["pulse"]
 handle_json(pulse)
