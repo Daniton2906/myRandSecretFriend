6fdaf333a39c76adcd9a6c83e6102cea016aa613
ddiomedi2096 <ddiomedi2906@gmail.com>
2019-08-24 16:27:49 -0400

    Add secret_friend logic, getId and get_friend Add list_members

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import random
 
+import datetime as dt
 from telegram.ext import Updater, CommandHandler
 import requests
 import re
@@ -7,10 +8,14 @@ import datetime
 import os
 import uuid
 import json
+import glob
+
 
 from config.auth import TOKEN
 
 import logging
+
+from bot_logic import secret_friend
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -25,21 +30,13 @@ def validate(date_text):
     except ValueError:
         raise ValueError("Incorrect data format, should be DD-MM-YYYY")
 
-def get_secret_friend(context):
-    """Send the alarm message."""
-    job = context.job
-    context.bot.send_message(job.context, text='Beep!')
-
-
-def test_secret_friend(update, context):
-    new_job = context.job_queue.run_once(get_secret_friend, due, context=chat_id)
-
-
-def create(bot, update, args):
+def create(bot, update, args, chat_data, job_queue):
     logger.info('He recibido un comando create')
     """Add a job to the queue."""
     chat_id = update.message.chat_id
-    my_id = update.message.to_dict()['from']['id']
+    message_dict = update.message.to_dict()
+    my_id = message_dict['from']['id']
+    username = message_dict['from']['username']
     try:
         # args[0] should contain the time for the timer in seconds
         due = args[0]
@@ -56,11 +53,12 @@ def create(bot, update, args):
 
         new_id = abs(chat_id)  # random.randint(1, 1000000000) # uuid.uuid4()
         filename = f"group_{new_id}.json"
-        new_dict = {"id": new_id, "members": [my_id], "date": due, "time": "00:00", "format": "utc", "state": "pending", "results": []}
+        new_dict = {"id": new_id, "members": [my_id], "usernames": [username], "date": due, "time": "00:00", "format": "utc", "state": "pending", "results": []}
 
         with open(path + filename, 'w') as outfile:
             json.dump(new_dict, outfile)
 
+        new_job = job_queue.run_once(get_secret_friend, 10, context=chat_id)
         update.message.reply_text(f'Grupo creado existosamente! El sorteo se hara el {due}')
 
     except (IndexError, ValueError):
@@ -79,7 +77,6 @@ def join(bot, update, args):
     group_id = abs(chat_id)
 
     filename = f"group_{group_id}.json"
-    # new_dict = {"id": new_id, "members": [my_id], "date" : due, "time": "00:00", "format": "utc", "state": "pending", "results": []}
 
     data = {}
     with open(path + filename) as json_file:
@@ -87,6 +84,7 @@ def join(bot, update, args):
 
     if my_id not in data["members"]:
         data["members"].append(my_id)
+        data["usernames"].append(username)
         with open(path + filename, 'w') as outfile:
             json.dump(data, outfile)
 
@@ -114,7 +112,9 @@ def leave(bot, update, args):
         data = json.load(json_file)
 
     if my_id in data["members"]:
+        idx = data["members"].index(my_id)
         data["members"].remove(my_id)
+        data["usernames"].pop(idx)
         with open(path + filename, 'w') as outfile:
             json.dump(data, outfile)
 
@@ -148,18 +148,96 @@ def finish(bot, update, args):
     else:
         update.message.reply_text(f'El grupo ya está cerrado (sorteo el {date}) >:|')
 
+def get_secret_friend(bot, job):
+    """Send the alarm message."""
+    group_id = abs(job.context)
+    filename = f"group_{group_id}.json"
+    run_secret_friend = False
+    with open("data/" + filename) as json_file:
+        data = json.load(json_file)
+        if dt.datetime.now() >= dt.datetime.strptime(data["date"], '%d-%m-%Y'):
+            run_secret_friend = True
+    if run_secret_friend:
+        job.schedule_removal()
+        secret_friend(filename)
+        bot.send_message(job.context, text='Beep!')
+    else:
+        bot.send_message(job.context, text='NoBeep!')
+
+
+def test_secret_friend(bot, update, args, chat_data, job_queue):
+    chat_id = update.message.chat_id
+    new_id = abs(chat_id)
+    new_job = job_queue.run_once(get_secret_friend, 10, context=chat_id)
+    update.message.reply_text('Esperando para correr!')
+
+def get_id(bot, update, args):
+    logger.info('He recibido un comando getId')
+    print(update.message)
+
+    chat_id = update.message.chat_id
+    message_dict = update.message.to_dict()
+    update.message.reply_text(f'El id del grupo es {abs(chat_id)} :)')
+
+def get_friend(bot, update, args):
+    logger.info('He recibido un comando getId')
+    print(update.message)
+
+    chat_id = update.message.chat_id
+    message_dict = update.message.to_dict()
+    my_id = message_dict['from']['id']
+    username = message_dict['from']['username']
+
+    if chat_id == my_id:
+        # llamar funcion
+        my_friend = 0
+        update.message.reply_text(f'Tu amike secreto es {my_friend} :)')
+    else:
+        update.message.reply_text(f'Preguntamelo por interno :*')
+
+def list_members(bot, update, args):
+    logger.info('He recibido un comando join')
+    print(update.message)
+
+    chat_id = update.message.chat_id
+    message_dict = update.message.to_dict()
+    my_id = message_dict['from']['id']
+    username = message_dict['from']['username']
+
+    path = "data/"
+    group_id = abs(chat_id)
+
+    filename = f"group_{group_id}.json"
+    # new_dict = {"id": new_id, "members": [my_id], "date" : due, "time": "00:00", "format": "utc", "state": "pending", "results": []}
+
+    data = {}
+    with open(path + filename) as json_file:
+        data = json.load(json_file)
+
+    if my_id not in data["members"]:
+        data["members"].append(my_id)
+        with open(path + filename, 'w') as outfile:
+            json.dump(data, outfile)
+
+        update.message.reply_text(f'El usuario {username} ha sido añadido al grupo!')
+    else:
+        update.message.reply_text(f'El usuario {username} ya ha sido agregado >:|')
+
+
 def main():
     logger.info('Bot inicializado')
     updater = Updater(TOKEN)
     dp = updater.dispatcher
     # dp.add_handler(CommandHandler('bop', bop))
     dp.add_handler(CommandHandler('create', create, pass_args=True,
-                                  pass_job_queue=True,
-                                  pass_chat_data=True))
+                                  pass_chat_data=True,
+                                  pass_job_queue=True,))
     dp.add_handler(CommandHandler('join', join, pass_args=True))
     dp.add_handler(CommandHandler('leave', leave, pass_args=True))
     dp.add_handler(CommandHandler('finish', finish, pass_args=True))
-    dp.add_handler(CommandHandler('test', test_secret_friend, pass_args=True))
+    dp.add_handler(CommandHandler('test', test_secret_friend, pass_args=True, pass_chat_data=True, pass_job_queue=True))
+    dp.add_handler(CommandHandler('getId', get_id, pass_args=True))
+    dp.add_handler(CommandHandler('getFriend', get_friend, pass_args=True))
     updater.start_polling()
     updater.idle()
 
