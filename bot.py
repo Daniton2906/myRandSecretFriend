316acefe13fb1a2a1a910497149cb47f9d3b0cd8
ddiomedi2096 <ddiomedi2906@gmail.com>
2019-08-24 12:10:05 -0400

    add base bot file

---
+++ bot.py
@@ -0,0 +1,37 @@
+from telegram.ext import Updater, CommandHandler
+import requests
+import re
+
+
+TOKEN = "943142793:AAGzV_GR4fcuh5o81Y67Gb9-oMQJOFIOOxk"
+
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()
+    url = contents['url']
+    return url
+
+def get_image_url():
+    allowed_extension = ['jpg','jpeg','png']
+    file_extension = ''
+    while file_extension not in allowed_extension:
+        url = get_url()
+        file_extension = re.search("([^.]*)$",url).group(1).lower()
+    return url
+
+def bop(bot, update):
+    url = get_image_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+    print("woaf")
+
+
+def main():
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('bop', bop))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
