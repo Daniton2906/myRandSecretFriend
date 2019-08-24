46fb319f9dbe6e8fa5b8bda2c3eb55964eedf825
Javi801 <javi@niclabs.cl>
2019-08-24 13:25:22 +0200

    Basic data

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import requests
+import datetime as dt
 
 def handle_json(data):
     print(data)
 
-# Get last pulse generated
-beacon_url = "https://beacon.clcert.cl/beacon/2.0/pulse/last"
+# Lista de usuarios en el grupo
+amigos = ['a', 'b', 'c', 'd', 'e']
+
+# fecha para el sorteo en formato dd-mm-aaaa
+date = "20-06-2020"
+time = "20:00"
 content = requests.get(beacon_url)
 pulse = content.json()["pulse"]
 handle_json(pulse)
