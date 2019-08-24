32fc8460a873ceee2a8af7aa7948a7a1a2311e79
Javi801 <javi@niclabs.cl>
2019-08-24 13:44:10 +0200

    language

--- main.py
+++ main.py
@@ -4,30 +4,32 @@ import datetime as dt
 def handle_json(data):
     print(data)
 
-# Lista de usuarios en el grupo
+# users list
 amigos = ['a', 'b', 'c', 'd', 'e']
 
-# fecha para el sorteo en formato dd-mm-aaaa
+# initial date given by the user
 date = "20-06-2020"
 time = "20:00"
 
-# verificacion de la fecha
+# Date verification
 correct = 0
 while (correct == 0):
     numero = input("Desea realizar el sorteo en la fecha y hora siguientes?: \n" + date + " " + time + "\n")
+    correct = int(numero)
     if (correct):
         break
-    date = raw_input("Ingresa la nueva fecha en formato dd-mm-aaaa:")
-    time = raw_input("Ingresa la nueva fecha en formato hh:mm :")
+    date = input("Ingresa la nueva fecha en formato dd-mm-aaaa:")
+    time = input("Ingresa la nueva fecha en formato hh:mm :")
 
-# TODO verificacion de formato
+# TODO date format verification
 # TODO zona horaria
 
-# fecha formato posix
+# Date in posix format
 posix_date = int(dt.datetime.strptime('{d} {t}'.format(d=date, t=time), '%d-%m-%Y %H:%M').timestamp())
 
 # Get pulse generated
 beacon_url = "https://beacon.clcert.cl/beacon/2.0/pulse/" + str(posix_date)
 content = requests.get(beacon_url)
 pulse = content.json()["pulse"]
+
 handle_json(pulse)
